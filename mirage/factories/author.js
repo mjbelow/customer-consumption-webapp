2c19b093665723fa54cc70522b8b18e2827e8317
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-02-22 12:16:08 -0500

    added book factory

--- mirage/factories/author.js
+++ mirage/factories/author.js
@@ -1,5 +1,9 @@
 import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  name: (i) => `Author #${i}`
+  name: (i) => `Author #${i}`,
+  afterCreate(author, server) {
+    // create 3-7 books for each author
+    server.createList('book', Math.floor(Math.random()*5)+3, { author });
+  }
 });
