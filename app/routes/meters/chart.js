6ec56007b9e5e277d65d3a14dda265e51a6a6560
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-03-09 18:15:02 -0400

    feat: make chart responsive

--- app/routes/meters/chart.js
+++ app/routes/meters/chart.js
@@ -43,24 +43,10 @@ export default Route.extend({
 
     let date = (this.get('date'));
 
-    let meterIntervals = model.meter.meterIntervals.toArray();
     let meterIntervalData = [];
-    {
-      let length = meterIntervals.length;
-      let i;
-      let count = 0;
-      for(i = 0; i < length; i++)
-      {
-        let readDateTime = meterIntervals[i].get('readDateTime');
-        if(readDateTime >= date) {
-          meterIntervalData.push(meterIntervals[i].get('readValue'));
-          count++;
-        }
-        if(count >= howMany) {
-          break;
-        }
-      }
-    }
+    model.meter.meterIntervals.forEach(data => {
+      meterIntervalData.push(data.get('readValue'));
+    })
 
     // set x labels based on raw data
     function xLabels(value) {
@@ -84,17 +70,6 @@ export default Route.extend({
       }
     }
 
-    // generate hours for x axes
-    function getHours(hour, arr) {
-      arr.length = 0;
-      let i;
-      for(i = 0; i < howMany; i++)
-      {
-        arr.push((hour+i)%24);
-      }
-      return arr;
-    }
-
     function getTemperatureData(hour, arr) {
       arr.length = 0;
       let i;
@@ -111,14 +86,60 @@ export default Route.extend({
       // meter data
       [meterIntervalData,[]],
       // time of day
-      getHours(this.get('hour'),[])
+      [
+        [
+          '12:00am', '1:00am',  '2:00am',
+          '3:00am',  '4:00am',  '5:00am',
+          '6:00am',  '7:00am',  '8:00am',
+          '9:00am',  '10:00am', '11:00am',
+          '12:00pm', '1:00pm',  '2:00pm',
+          '3:00pm',  '4:00pm',  '5:00pm',
+          '6:00pm',  '7:00pm',  '8:00pm',
+          '9:00pm',  '10:00pm', '11:00pm'
+        ],
+        [
+          '12:00am', '12:30am', '1:00am',  '1:30am',
+          '2:00am',  '2:30am',  '3:00am',  '3:30am',
+          '4:00am',  '4:30am',  '5:00am',  '5:30am',
+          '6:00am',  '6:30am',  '7:00am',  '7:30am',
+          '8:00am',  '8:30am',  '9:00am',  '9:30am',
+          '10:00am', '10:30am', '11:00am', '11:30am',
+          '12:00pm', '12:30pm', '1:00pm',  '1:30pm',
+          '2:00pm',  '2:30pm',  '3:00pm',  '3:30pm',
+          '4:00pm',  '4:30pm',  '5:00pm',  '5:30pm',
+          '6:00pm',  '6:30pm',  '7:00pm',  '7:30pm',
+          '8:00pm',  '8:30pm',  '9:00pm',  '9:30pm',
+          '10:00pm', '10:30pm', '11:00pm', '11:30pm'
+        ],
+        [
+          '12:00am', '12:15am', '12:30am', '12:45am', '1:00am',  '1:15am',
+          '1:30am',  '1:45am',  '2:00am',  '2:15am',  '2:30am',  '2:45am',
+          '3:00am',  '3:15am',  '3:30am',  '3:45am',  '4:00am',  '4:15am',
+          '4:30am',  '4:45am',  '5:00am',  '5:15am',  '5:30am',  '5:45am',
+          '6:00am',  '6:15am',  '6:30am',  '6:45am',  '7:00am',  '7:15am',
+          '7:30am',  '7:45am',  '8:00am',  '8:15am',  '8:30am',  '8:45am',
+          '9:00am',  '9:15am',  '9:30am',  '9:45am',  '10:00am', '10:15am',
+          '10:30am', '10:45am', '11:00am', '11:15am', '11:30am', '11:45am',
+          '12:00pm', '12:15pm', '12:30pm', '12:45pm', '1:00pm',  '1:15pm',
+          '1:30pm',  '1:45pm',  '2:00pm',  '2:15pm',  '2:30pm',  '2:45pm',
+          '3:00pm',  '3:15pm',  '3:30pm',  '3:45pm',  '4:00pm',  '4:15pm',
+          '4:30pm',  '4:45pm',  '5:00pm',  '5:15pm',  '5:30pm',  '5:45pm',
+          '6:00pm',  '6:15pm',  '6:30pm',  '6:45pm',  '7:00pm',  '7:15pm',
+          '7:30pm',  '7:45pm',  '8:00pm',  '8:15pm',  '8:30pm',  '8:45pm',
+          '9:00pm',  '9:15pm',  '9:30pm',  '9:45pm',  '10:00pm', '10:15pm',
+          '10:30pm', '10:45pm', '11:00pm', '11:15pm', '11:30pm', '11:45pm'
+        ]
+      ]
     ]
 
     trimData(data[0], 0, 100);
     trimData(data[1], .5);
+    let n = [6];
+
+    let prevDataCount = 0;
 
     let chartData = {
-      labels: data[2],
+      labels: data[2][0].slice(0,6),
       datasets: [{
         yAxisID: 'temperature',
         label: 'Temperature (F°)',
@@ -186,7 +207,7 @@ export default Route.extend({
             return `${data[tooltipItem.datasetIndex][0][tooltipItem.index]} ${uom}`;
           },
           title: function(tooltipItems) {
-            return xLabels(tooltipItems[0].xLabel);
+            // return xLabels(tooltipItems[0].xLabel);
           }
         }
       },
@@ -226,7 +247,7 @@ export default Route.extend({
         xAxes: [
           {
             ticks: {
-              callback: xLabels
+              // callback: xLabels
             }
           }
         ]
@@ -261,10 +282,35 @@ export default Route.extend({
           let increment = Math.floor(max/20) + 1;
           this.options.scales.yAxes[0].ticks.max = Math.ceil((max+increment));
           this.options.scales.yAxes[0].ticks.stepSize = this.options.scales.yAxes[0].ticks.max / 6;
-
+          
+          
           this.update();
         }
 
+      },
+      onResize: function(chart, size) {
+        let dataCount;
+        
+        if(size.width > 2116)
+          dataCount = 36;
+        else if(size.width > 1786)
+          dataCount = 30;
+        else if(size.width > 1456)
+          dataCount = 24;
+        else if(size.width > 1126)
+          dataCount = 18;
+        else if(size.width > 796)
+          dataCount = 12;
+        else if(size.width > 466)
+          dataCount = 6;
+        else
+          dataCount = 3;
+        
+        if(dataCount != prevDataCount) {
+          prevDataCount = dataCount;
+          chartData.labels = data[2][0].slice(0,dataCount);
+          chart.update();
+        }
       }
     }
 
